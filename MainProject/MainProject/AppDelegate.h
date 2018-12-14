f48c44fdb03c5504419cb59c8f85b6438ccb82f7
DJ <djxu1233@163.com>
2018-12-14 16:40:41 +0800

    提交

---
+++ MainProject/MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by DJ on 2018/12/14.
+//  Copyright © 2018年 DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by DJ on 2018/12/14.
+//  Copyright © 2018年 DJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
